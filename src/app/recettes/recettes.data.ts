2e6f2efa8cf05643af67c35a9fe04d82e6124083
bo <bo.zheng@ext.adeo.com>
2025-06-27 16:15:33 +0200

    modified:   src/404.html 	modified:   src/app/recettes/recettes.data.ts 	modified:   src/main.ts

--- src/app/recettes/recettes.data.ts
+++ src/app/recettes/recettes.data.ts
@@ -6,15 +6,15 @@ export interface Recette {
 export const RECETTES: Recette[] = [
   {
     nom: 'Nouilles',
-    image: 'assets/images/nouilles-sautees-aux-legumes.jpg'
+    image: '/assets/images/nouilles-sautees-aux-legumes.jpg'
   },
   {
     nom: 'Riz',
-    image: 'assets/images/riz-cantonais.webp'
+    image: '/assets/images/riz-cantonais.webp'
   },
   {
     nom: 'Poulet',
-    image: 'assets/images/poulet-kung-bao.jpg'
+    image: '/assets/images/poulet-kung-bao.jpg'
   }
   // Ajoute d'autres recettes ici
 ];
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)],
+  providers: [
+    provideRouter(routes),
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
+  ],
 })
   .catch((err) => console.error(err));
